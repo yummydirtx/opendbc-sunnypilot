225686160627db9b3695891d6dcfebe63138c628
Alex Frutkin <frutkinalexander@gmail.com>
2026-04-12 16:16:38 -0700

    mazda: update ACC availability source in alpha-long mode

--- opendbc/car/mazda/carstate.py
+++ opendbc/car/mazda/carstate.py
@@ -124,10 +124,10 @@ class CarState(CarStateBase):
       *create_button_events(self.main_button, prev_main_button, {1: ButtonType.mainCruise}),
     ]
 
-    # In alpha-long mode the radar-owned CRZ_CTRL frame is intentionally
-    # suppressed, but the ACC state still survives on PEDALS.
+    # In alpha-long mode ACC activity still survives on PEDALS, but
+    # availability remains sourced from CRZ_CTRL.
     if self.CP.openpilotLongitudinalControl:
-      ret.cruiseState.available = cp.vl["PEDALS"]["CRZ_AVAILABLE"] == 1
+      ret.cruiseState.available = cp.vl["CRZ_CTRL"]["CRZ_AVAILABLE"] == 1
       ret.cruiseState.enabled = cp.vl["PEDALS"]["ACC_ACTIVE"] == 1
     else:
       # TODO: the signal used for available seems to be the adaptive cruise signal,
